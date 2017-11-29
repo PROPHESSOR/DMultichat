a8d2c55b68b55a8a1af5668e8b3e6d1e0ab75c91
PROPHESSOR <xenteze@gmail.com>
2017-11-29 13:47:18 +0200

    Обновление логгера (система коллбэков)

--- libs/logger.js
+++ libs/logger.js
@@ -9,13 +9,14 @@
  * });
  * Log.log("message");
  * Log.log("message", {noconvert:true});
-*/
-/* eslint-disable */
-'use strict';
+ */
+// /* eslint-disable */
+
+"use strict";
 
 /* eslint-disable no-console */
 
-const chalk = require('chalk');
+const chalk = require("chalk");
 
 class Logger {
 	constructor() {
@@ -25,42 +26,63 @@ class Logger {
 		this.error = this.error.bind(this);
 		this.info = this.info.bind(this);
 		this.timestamp = this.timestamp.bind(this);
+		this.setLevels = this.setLevels.bind(this);
+		this.removeLevel = this.removeLevel.bind(this);
+		this.setCallback = this.setCallback.bind(this);
 
 		this.levels = [];
+		this.callbacks = {
+			log() {},
+			error() {},
+			warn() {},
+			info() {}
+		}
 	}
 
 	log(data, options = {}) {
 		if (options.level && this.levels.indexOf(options.level) < 0) return;
-		let out = !options.noconvert ? this.convert(data) : data;
+		const out = !options.noconvert ? this.convert(data) : data;
 
 		console.log(chalk.cyan(out));
+		this.callbacks.log(out);
+
+		return this;
 	}
 
 	warn(data, options = {}) {
 		if (options.level && this.levels.indexOf(options.level) < 0) return;
-		let out = !options.noconvert ? this.convert(data) : data;
+		const out = !options.noconvert ? this.convert(data) : data;
 
 		console.log(chalk.yellow(out));
+		this.callbacks.warn(out);
+
+		return this;
 	}
 
 	error(data, options = {}) {
 		if (options.level && this.levels.indexOf(options.level) < 0) return;
-		let out = !options.noconvert ? this.convert(data) : data;
+		const out = !options.noconvert ? this.convert(data) : data;
 
 		console.log(chalk.red(out));
+		this.callbacks.error(out);
+
+		return this;
 	}
 
 	info(data, options = {}) {
 		if (options.level && this.levels.indexOf(options.level) < 0) return;
-		let out = !options.noconvert ? this.convert(data) : data;
+		const out = !options.noconvert ? this.convert(data) : data;
 
 		console.log(chalk.blue(out));
+		this.callbacks.info(out);
+
+		return this;
 	}
 
 	convert(data) {
 		let out = data;
 
-		if (typeof out === 'object') out = JSON.stringify(out);
+		if (typeof out === "object") out = JSON.stringify(out);
 
 		return this.timestamp(out);
 
@@ -82,6 +104,13 @@ class Logger {
 		if (idx >= 0) {
 			this.levels.splice(idx);
 		}
+		return this;
+	}
+
+	setCallback(event, callback = () => {}){
+		if(!this.callbacks[event]) return this.error(`Неверное действие ${event}`);
+		this.callbacks[event] = callback;
+		return this;
 	}
 
 }
