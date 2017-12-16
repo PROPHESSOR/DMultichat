11b086f15fa007cb3d13ebc5612f36cd4471f031
PROPHESSOR <xenteze@gmail.com>
2017-12-16 14:13:54 +0200

    Новая обьектно-ориентированная система API

---
+++ api/API.js
@@ -0,0 +1,63 @@
+/* API Class
+ * Don't remove!
+ * Copyright (c) 2017 PROPHESSOR
+*/
+
+"use strict";
+
+const {log, warn} = require("../libs/logger");
+
+class API {
+
+	/**
+	 * @constructor
+	 * @param {string} name - API name
+	 */
+	constructor(name) {
+		this._name = name;
+		this._isReady = false;
+
+		this.initialize = this.initialize.bind(this);
+		this.ready = this.ready.bind(this);
+		this.getNewMessages = this.getNewMessages.bind(this);
+		this.isReady = this.isReady.bind(this);
+
+		log(`${this._name} API connecting!`, {"level": "Api"});
+	}
+
+	/** Get current API name
+	 * @returns {string} API name
+	 */
+	get name() {
+		return this._name;
+	}
+
+	/** This method will executed at start
+	 * @returns {undefined} - Nothing
+	 */
+	initialize() {
+		warn(`In ${this.name} API "initialize" isn't implemented!`);
+	}
+
+	/** This method will executed on connect
+	 * @returns {undefined} - Nothing
+	 */
+	ready() {
+		warn(`In ${this.name} API "ready" isn't implemented!`);
+	}
+
+	/** This method must return array of messages
+	 * @returns {array} - Messages
+	 */
+	getNewMessages() {
+		warn(`In ${this.name} API "getNewMessages" isn't implemented!`);
+	}
+	/** Is API ready?
+	 * @returns {bool} True/false
+	 */
+	isReady() {
+		return this._isReady;
+	}
+}
+
+module.exports = API;
