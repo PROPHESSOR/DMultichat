401ad7263d2ff0c13ab76c849fedd717f268131e
PROPHESSOR <xenteze@gmail.com>
2018-01-31 22:20:01 +0200

    YouTube API переписан под ООП

--- api/youtube-api.js
+++ api/youtube-api.js
@@ -1,9 +1,13 @@
 "use strict";
 
+/* eslint-disable class-methods-use-this */
+
 const fs = require("fs");
 const google = require("googleapis");
 const googleAuth = require("google-auth-library");
 const winston = require("../libs/logger");
+
+const API = require("./API");
 
 const SCOPES = [
     "https://www.googleapis.com/auth/youtube.readonly",
@@ -14,290 +18,323 @@ const TOKEN_DIR = ".credentials/";
 const TOKEN_PATH = `${TOKEN_DIR}youtube-credentials.json`;
 
 const _youtube = google.youtube("v3");
-let _liveChatId = "";
-let _isReady = false;
-let _lastCheckTime = new Date().getTime();
-let _auth = null;
 const _userColorsMap = {};
-let _newMessages = [];
-let _liveBroadcastPollingStarted = false;
-let _messagePollingStarted = false;
-let _noLiveBroadcastFound = false;
-let _status = "off";
 
-function getRandomColor() {
-    const letters = "0123456789ABCDEF".split("");
-    let color = "#";
+class YoutubeAPI extends API {
+    constructor() {
+        super("YouTube");
 
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
+        this._newMessages = [];
+        this._status = "off";
+
+        this._liveChatId = "";
+        this._isReady = false;
+        this._lastCheckTime = new Date().getTime();
+        this._auth = null;
+        this._newMessages = [];
+        this._liveBroadcastPollingStarted = false;
+        this._messagePollingStarted = false;
+        this._noLiveBroadcastFound = false;
+
+        // Bind this
+        {
+            this.getRandomColor = this.getRandomColor.bind(this);
+            this.initialize = this.initialize.bind(this);
+            this.ready = this.ready.bind(this);
+            this.isReady = this.isReady.bind(this);
+            this.authorize = this.authorize.bind(this);
+            this.getTokenLink = this.getTokenLink.bind(this);
+            this.getNewToken = this.getNewToken.bind(this);
+            this.refreshToken = this.refreshToken.bind(this);
+            this.getToken = this.getToken.bind(this);
+            this.storeToken = this.storeToken.bind(this);
+            this.getLiveBroadcast = this.getLiveBroadcast.bind(this);
+            this.getNewMessages = this.getNewMessages.bind(this);
+            this.getChatMessages = this.getChatMessages.bind(this);
+            this.startLiveBroadcastPolling = this.startLiveBroadcastPolling.bind(this);
+            this.startMessagePolling = this.startMessagePolling.bind(this);
+        }
     }
 
-    return color;
-}
+    get status() {
+        return this._status;
+    }
 
-function initialize(config) {
-    authorize(config);
-}
+    getRandomColor() {
+        const letters = "0123456789ABCDEF".split("");
+        let color = "#";
 
-function ready() {
-    winston.info("Youtube API is ready to use", {"source": "youtube"});
-    _isReady = true;
-    _status = "ready";
-
-    _newMessages.push({
-        "type": "system",
-        "source": "youtube",
-        "date": new Date().getTime(),
-        "message": "ready|true"
-    });
-
-    startMessagePolling();
-}
-
-function isReady() {
-    return _isReady;
-}
-
-function authorize(credentials) {
-    const clientSecret = credentials.live_data.youtube.client_secret;
-    const clientId = credentials.live_data.youtube.client_id;
-    const redirectUrl = `${credentials.server.host}:${credentials.server.port}${credentials.live_data.youtube.redirect_url}`;
-
-    const auth = new googleAuth();
-
-    _auth = new auth.OAuth2(clientId, clientSecret, redirectUrl);
-
-    // Check if we have previously stored a token.
-    fs.readFile(TOKEN_PATH, (err, token) => {
-        if (err) {
-            getNewToken();
-        } else {
-            _auth.credentials = JSON.parse(token);
-            winston.info("Get stored token", {"source": "youtube"});
-
-            startLiveBroadcastPolling();
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
         }
-    });
-}
 
-function getTokenLink() {
-    if (!_auth) {
-        const config = require("../config.json") // FIXME: Костыль, но без него пока никак
-        const clientSecret = config.live_data.youtube.client_secret;
-        const clientId = config.live_data.youtube.client_id;
-        const redirectUrl = `${config.server.host}:${config.server.port}${config.live_data.youtube.redirect_url}`;
+        return color;
+    }
+
+    initialize(config) {
+        this.authorize(config);
+    }
+
+    ready() {
+        winston.info("Youtube API is ready to use", {"source": "youtube"});
+        this._isReady = true;
+        this._status = "ready";
+
+        this._newMessages.push({
+            "type": "system",
+            "source": "youtube",
+            "date": new Date().getTime(),
+            "message": "ready|true"
+        });
+
+        this.startMessagePolling();
+    }
+
+    isReady() {
+        return this._isReady;
+    }
+
+    authorize(credentials) {
+        const clientSecret = credentials.live_data.youtube.client_secret;
+        const clientId = credentials.live_data.youtube.client_id;
+        const redirectUrl = `${credentials.server.host}:${credentials.server.port}${credentials.live_data.youtube.redirect_url}`;
 
         const auth = new googleAuth();
 
-        _auth = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+        this._auth = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+
+        // Check if we have previously stored a token.
+        fs.readFile(TOKEN_PATH, (err, token) => {
+            if (err) {
+                this.getNewToken();
+            } else {
+                this._auth.credentials = JSON.parse(token);
+                winston.info("Get stored token", {"source": "youtube"});
+
+                this.startLiveBroadcastPolling();
+            }
+        });
     }
 
-    return _auth.generateAuthUrl({
-        "access_type": "offline",
-        "scope": SCOPES,
-        "approval_prompt": "force"
-    });
-}
+    getTokenLink() {
+        if (!this._auth) {
+            const config = require("../config.json") // FIXME: Костыль, но без него пока никак
+            const clientSecret = config.live_data.youtube.client_secret;
+            const clientId = config.live_data.youtube.client_id;
+            const redirectUrl = `${config.server.host}:${config.server.port}${config.live_data.youtube.redirect_url}`;
 
-function getNewToken() {
-    const authUrl = _auth.generateAuthUrl({
-        "access_type": "offline",
-        "scope": SCOPES,
-        "approval_prompt": "force"
-    });
+            const auth = new googleAuth();
 
-    winston.info("Please select your Youtube account to get a token and use the API.", {"source": "youtube"});
-
-    _newMessages.push({
-        "type": "system",
-        "source": "youtube",
-        "date": new Date().getTime(),
-        "message": `auth-url|${authUrl}`
-    });
-}
-
-function refreshToken() {
-    winston.info("Refresh token", {"source": "youtube"});
-
-    _auth.refreshAccessToken((err, token) => {
-        if (err) {
-            winston.error(`Error trying to get a refreshed token: ${err}`, {"source": "youtube"})
-        } else {
-            _auth.credentials = token;
-            storeToken(token);
-
-            startLiveBroadcastPolling();
-        }
-    })
-}
-
-function getToken(code) {
-    _auth.getToken(code, (err, token) => {
-        if (err) {
-            winston.error("Error while trying to retrieve access token", {"source": "youtube"});
-            winston.error(err, {"source": "youtube"});
-
-            return;
+            this._auth = new auth.OAuth2(clientId, clientSecret, redirectUrl);
         }
 
-        winston.info(token, {"source": "youtube"});
-        _auth.credentials = token;
-        storeToken(token);
-
-        startLiveBroadcastPolling();
-    });
-}
-
-function storeToken(token) {
-    try {
-        fs.mkdirSync(TOKEN_DIR);
-    } catch (err) {
-        if (err.code !== "EEXIST") {
-            throw err;
-        }
+        return this._auth.generateAuthUrl({
+            "accessthis._type": "offline",
+            "scope": SCOPES,
+            "approvalthis._prompt": "force"
+        });
     }
 
-    fs.writeFile(TOKEN_PATH, JSON.stringify(token));
-    winston.info(`Token stored to ${TOKEN_PATH}`, {"source": "youtube"});
-}
+    getNewToken() {
+        const authUrl = this._auth.generateAuthUrl({
+            "accessthis._type": "offline",
+            "scope": SCOPES,
+            "approvalthis._prompt": "force"
+        });
 
-function getLiveBroadcast() {
-    _youtube.liveBroadcasts.list({
-        "auth": _auth,
-        "part": "snippet",
-        "broadcastStatus": "active",
-        "broadcastType": "all"
-    }, (error, response) => {
-        if (error) {
-            winston.error(`The API returned an error: ${error}`, {"source": "youtube"});
-            getNewToken();
-        } else {
-            const liveBroadcasts = response.items;
+        winston.info("Please select your Youtube account to get a token and use the API.", {"source": "youtube"});
 
-            if (liveBroadcasts.length > 0) {
-                if (_isReady) return;
+        this._newMessages.push({
+            "type": "system",
+            "source": "youtube",
+            "date": new Date().getTime(),
+            "message": `auth-url|${authUrl}`
+        });
+    }
 
-                _noLiveBroadcastFound = false;
-                _status = "ready";
-                const [liveBroadcast] = liveBroadcasts;
+    refreshToken() {
+        winston.info("Refresh token", {"source": "youtube"});
 
-                winston.info("Live broadcast found", {"source": "youtube"});
-                winston.info(`Title: ${liveBroadcast.snippet.title}`, {"source": "youtube"});
-                winston.info(`Description: ${liveBroadcast.snippet.description}`, {"source": "youtube"});
+        this._auth.refreshAccessToken((err, token) => {
+            if (err) {
+                winston.error(`Error trying to get a refreshed token: ${err}`, {"source": "youtube"})
+            } else {
+                this._auth.credentials = token;
+                this.storeToken(token);
 
-                _liveChatId = liveBroadcast.snippet.liveChatId;
+                this.startLiveBroadcastPolling();
+            }
+        })
+    }
 
-                ready();
-            } else if (!_noLiveBroadcastFound) {
-                if (_isReady) {
-                    _isReady = false;
+    getToken(code) {
+        this._auth.getToken(code, (err, token) => {
+            if (err) {
+                winston.error("Error while trying to retrieve access token", {"source": "youtube"});
+                winston.error(err, {"source": "youtube"});
 
-                    _newMessages.push({
+                return;
+            }
+
+            winston.info(token, {"source": "youtube"});
+            this._auth.credentials = token;
+            this.storeToken(token);
+
+            this.startLiveBroadcastPolling();
+        });
+    }
+
+    storeToken(token) {
+        try {
+            fs.mkdirSync(TOKEN_DIR);
+        } catch (err) {
+            if (err.code !== "EEXIST") {
+                throw err;
+            }
+        }
+
+        fs.writeFile(TOKEN_PATH, JSON.stringify(token));
+        winston.info(`Token stored to ${TOKEN_PATH}`, {"source": "youtube"});
+    }
+
+    getLiveBroadcast() {
+        _youtube.liveBroadcasts.list({
+            "auth": this._auth,
+            "part": "snippet",
+            "broadcastStatus": "active",
+            "broadcastType": "all"
+        }, (error, response) => {
+            if (error) {
+                winston.error(`The API returned an error: ${error}`, {"source": "youtube"});
+                this.getNewToken();
+            } else {
+                const liveBroadcasts = response.items;
+
+                if (liveBroadcasts.length > 0) {
+                    if (this._isReady) return;
+
+                    this._noLiveBroadcastFound = false;
+                    this._status = "ready";
+                    const [liveBroadcast] = liveBroadcasts;
+
+                    winston.info("Live broadcast found", {"source": "youtube"});
+                    winston.info(`Title: ${liveBroadcast.snippet.title}`, {"source": "youtube"});
+                    winston.info(`Description: ${liveBroadcast.snippet.description}`, {"source": "youtube"});
+
+                    this._liveChatId = liveBroadcast.snippet.liveChatId;
+
+                    this.ready();
+                } else if (!this._noLiveBroadcastFound) {
+                    if (this._isReady) {
+                        this._isReady = false;
+
+                        this._newMessages.push({
+                            "type": "system",
+                            "source": "youtube",
+                            "date": new Date().getTime(),
+                            "message": "ready|false"
+                        });
+                    }
+
+                    this._noLiveBroadcastFound = true;
+                    this._status = "nobroadcast";
+                    const errorMessage = "No broadcast live detected";
+
+                    winston.error(errorMessage, {"source": "youtube"});
+
+                    this._newMessages.push({
                         "type": "system",
                         "source": "youtube",
                         "date": new Date().getTime(),
-                        "message": "ready|false"
+                        "message": `error|${errorMessage}`
                     });
                 }
-
-                _noLiveBroadcastFound = true;
-                _status = "nobroadcast";
-                const errorMessage = "No broadcast live detected";
-
-                winston.error(errorMessage, {"source": "youtube"});
-
-                _newMessages.push({
-                    "type": "system",
-                    "source": "youtube",
-                    "date": new Date().getTime(),
-                    "message": `error|${errorMessage}`
-                });
             }
-        }
-    });
-}
+        });
+    }
 
-function getNewMessages() {
-    if (_newMessages.length === 0) return [];
+    getNewMessages() {
+        if (this._newMessages.length === 0) return [];
 
-    const newMessage = _newMessages;
+        const newMessage = this._newMessages;
 
-    _newMessages = [];
+        this._newMessages = [];
 
-    return newMessage;
-}
+        return newMessage;
+    }
 
-function getChatMessages() {
-    if (!_isReady) return;
+    getChatMessages() {
+        if (!this._isReady) return;
 
-    _youtube.liveChatMessages.list({
-        "auth": _auth,
-        "part": "snippet,authorDetails",
-        "liveChatId": _liveChatId
-    }, (error, response) => {
-        if (error) {
-            winston.error(`The API returned an error: ${error}`, {"source": "youtube"});
-            refreshToken();
+        _youtube.liveChatMessages.list({
+            "auth": this._auth,
+            "part": "snippet,authorDetails",
+            "liveChatId": this._liveChatId
+        }, (error, response) => {
+            if (error) {
+                winston.error(`The API returned an error: ${error}`, {"source": "youtube"});
+                this.refreshToken();
 
-            return;
-        }
+                return;
+            }
 
-        const messages = response.items;
-        const chatMessages = [];
+            const messages = response.items;
+            const chatMessages = [];
 
-        if (messages.length > 0) {
-            for (let i = 0; i < messages.length; i++) {
-                const message = messages[i];
+            if (messages.length > 0) {
+                for (let i = 0; i < messages.length; i++) {
+                    const message = messages[i];
 
-                if (message.snippet.type === "textMessageEvent") {
-                    const messageTimestamp = new Date(message.snippet.publishedAt).getTime();
+                    if (message.snippet.type === "textMessageEvent") {
+                        const messageTimestamp = new Date(message.snippet.publishedAt).getTime();
 
-                    if (_lastCheckTime < messageTimestamp) {
-                        const author = message.authorDetails.displayName;
+                        if (this._lastCheckTime < messageTimestamp) {
+                            const author = message.authorDetails.displayName;
 
-                        if (!(author in _userColorsMap)) _userColorsMap[author] = getRandomColor();
+                            if (!(author in _userColorsMap)) _userColorsMap[author] = this.getRandomColor();
 
-                        const chatMessage = {
-                            "type": "chat",
-                            author,
-                            "message": message.snippet.textMessageDetails.messageText,
-                            "source": "youtube",
-                            "date": messageTimestamp,
-                            "color": _userColorsMap[author]
-                        };
+                            const chatMessage = {
+                                "type": "chat",
+                                author,
+                                "message": message.snippet.textMessageDetails.messageText,
+                                "source": "youtube",
+                                "date": messageTimestamp,
+                                "color": _userColorsMap[author]
+                            };
 
-                        chatMessages.push(chatMessage);
+                            chatMessages.push(chatMessage);
+                        }
                     }
                 }
-            }
 
-            if (chatMessages.length > 0) {
-                _newMessages = _newMessages.concat(chatMessages);
-                _lastCheckTime = chatMessages[chatMessages.length - 1].date;
+                if (chatMessages.length > 0) {
+                    this._newMessages = this._newMessages.concat(chatMessages);
+                    this._lastCheckTime = chatMessages[chatMessages.length - 1].date;
+                }
             }
+        });
+    }
+
+    startLiveBroadcastPolling() {
+        if (!this._liveBroadcastPollingStarted) {
+            this._liveBroadcastPollingStarted = true;
+            setInterval(this.getLiveBroadcast, 1000);
         }
-    });
-}
+    }
 
-function startLiveBroadcastPolling() {
-    if (!_liveBroadcastPollingStarted) {
-        _liveBroadcastPollingStarted = true;
-        setInterval(getLiveBroadcast, 1000);
+    startMessagePolling() {
+        if (!this._messagePollingStarted) {
+            this._messagePollingStarted = true;
+            setInterval(this.getChatMessages, 1000);
+        }
     }
 }
 
-function startMessagePolling() {
-    if (!_messagePollingStarted) {
-        _messagePollingStarted = true;
-        setInterval(getChatMessages, 1000);
-    }
-}
-
-exports.initialize = initialize;
-exports.isReady = isReady;
-exports.getToken = getToken;
-exports.getNewMessages = getNewMessages;
-exports.getTokenLink = getTokenLink;
-exports.SCOPES = SCOPES;
-exports.status = _status;
+module.exports = new YoutubeAPI();
+// exports.initialize = initialize;
+// exports.isReady = isReady;
+// exports.getToken = getToken;
+// exports.getNewMessages = getNewMessages;
+// exports.getTokenLink = getTokenLink;
+// exports.SCOPES = SCOPES;
+// exports.status = this._status;
